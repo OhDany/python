fde036ac5693fdd0d97dc87ffc810289cc38699d
Daniel Ortega <oh.dany.mx@gmail.com>
2020-06-12 19:15:29 -0500

    Conceptos Tipos de datos Listas y metodos free

--- lists.py
+++ lists.py
@@ -3,71 +3,71 @@
 basket = [1, 2, 3, 4, 5]
 # print(len(basket))
 
-# # adding
-# basket.append(6)
-# basket.insert(2, 100)
-# new_list = basket
-# print(new_list)
-#
-# extendido = [1, 2, 3, 4, 5]
-# new_extendido = extendido.extend([7, 8])
-# print(extendido)
-# print(new_extendido)
-#
-# # remove
-# remove_pop = [1, 2, 3, 4, 5]
-# remove_pop.pop()
-# print('pop', remove_pop)
-#
-# remove_remove = [1, 2, 3, 4, 5]
-# remove_remove.remove(3)
-# print('remove', remove_remove)
-#
-# remove_clear = [1, 2, 3, 4, 5]
-# remove_clear.clear()
-# print('clear', remove_clear)
+# adding
+basket.append(6)
+basket.insert(2, 100)
+new_list = basket
+print(new_list)
 
-# # index
-# print(basket.index(1))
+extendido = [1, 2, 3, 4, 5]
+new_extendido = extendido.extend([7, 8])
+print(extendido)
+print(new_extendido)
 
-# # in
-# print(1 in basket)
+# remove
+remove_pop = [1, 2, 3, 4, 5]
+remove_pop.pop()
+print('pop', remove_pop)
 
-# # count
-# word = "This is The magic word"
-# print(word.count('T'))
+remove_remove = [1, 2, 3, 4, 5]
+remove_remove.remove(3)
+print('remove', remove_remove)
 
-# # sort
-# lista = ['q', 'r', 'b', 'a', 'f']
-#
-# #lista.sort()
-# print(lista)
-#
-# #reverse
-# lista.reverse()
-# print(lista)
+remove_clear = [1, 2, 3, 4, 5]
+remove_clear.clear()
+print('clear', remove_clear)
 
-# #range
-# listas = list(range(1,100))
-#
-# for lista in listas:
-#     print(lista)
+# index
+print(basket.index(1))
 
-# # join
-# sentence = ' '
-# new_sentence = sentence.join(['Hi', 'Dany', 'como', 'estas'])
-#
-# new_sentence2 = ' '.join(['Hi', 'Dany', 'como', 'estas'])
-# print(new_sentence)
-# print(new_sentence2)
+# in
+print(1 in basket)
 
-# list unpacking
-# a, b, c, *other, d = [1, 2, 3, 5, 7, 8, 9]
-# print(a)
-# print(b)
-# print(c)
-# print(other)
-# print(d)
+# count
+word = "This is The magic word"
+print(word.count('T'))
+
+# sort
+lista = ['q', 'r', 'b', 'a', 'f']
+
+#lista.sort()
+print(lista)
+
+#reverse
+lista.reverse()
+print(lista)
+
+#range
+listas = list(range(1,100))
+
+for lista in listas:
+    print(lista)
+
+# join
+sentence = ' '
+new_sentence = sentence.join(['Hi', 'Dany', 'como', 'estas'])
+
+new_sentence2 = ' '.join(['Hi', 'Dany', 'como', 'estas'])
+print(new_sentence)
+print(new_sentence2)
+
+list unpacking
+a, b, c, *other, d = [1, 2, 3, 5, 7, 8, 9]
+print(a)
+print(b)
+print(c)
+print(other)
+print(d)
 
 # None
 wapons = None
